028f212373780442734bf0b335f1d49e23788418
chenjunjie <610441295@qq.com>
2023-12-18 16:05:58 +0800

    try: use reward shaping in goals

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
-# # PPO
-# python3 ppo.py --exp_name PPO --env_id 'NSL/NextStationLondon-v0' --total_timesteps 2000000 --num_steps 50 --num_envs 12
+# PPO
+python3 ppo.py --exp_name PPO --env_id 'NSL/NextStationLondon-v0' --total_timesteps 2000000 --num_steps 50 --num_envs 12
 
 
-# DQN
-python3 dqn.py --exp_name DQN --env_id 'NSL/NextStationLondon-v0' --total_timesteps 2000000
+# # DQN
+# python3 dqn.py --exp_name DQN --env_id 'NSL/NextStationLondon-v0' --total_timesteps 2000000
